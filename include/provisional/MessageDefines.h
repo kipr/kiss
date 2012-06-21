398ce83790f1d41c41faaf9a54c328b28c3725af
Braden McDorman <bmcdorman@gmail.com>
2012-06-21 12:47:25 -0500

    Deleting old files

--- include/provisional/MessageDefines.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MESSAGEDEFINES_H_
-#define _MESSAGEDEFINES_H_
-
-#define MESSAGE_AVAILABLE 1
-#define MESSAGE_INFORMATION 2
-#define MESSAGE_DOWNLOAD 3
-#define MESSAGE_RUN 4
-
-#endif
--- src/provisional/DeviceInterface.cpp
+++
@@ -1,4 +0,0 @@
-#include "DeviceInterface.h"
-
-DeviceInterface::DeviceInterface(const QString& name)
-	: Named(name) {}
