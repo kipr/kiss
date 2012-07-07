4f06dcb201073b387388331e8248027760caf25c
Braden McDorman <bmcdorman@gmail.com>
2012-07-07 14:20:33 -0500

    Whitespace normalization

--- src/project/ProjectSettingsTab.cpp
+++ src/project/ProjectSettingsTab.cpp
@@ -15,86 +15,86 @@
 class ComboDelegate : public QStyledItemDelegate
 {
 public:
-    ComboDelegate(QObject *parent, QStringList keys)
-        : QStyledItemDelegate(parent) {
-        this->keys = keys;
-    }
+	ComboDelegate(QObject *parent, QStringList keys)
+		: QStyledItemDelegate(parent) {
+		this->keys = keys;
+	}
 
-    ~ComboDelegate() {
+	~ComboDelegate() {
 
-    }
+	}
 
-    virtual QWidget* createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const {
-        if(index.column() != 0)
-            return QStyledItemDelegate::createEditor(parent, option, index);
+	virtual QWidget* createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const {
+		if(index.column() != 0)
+			return QStyledItemDelegate::createEditor(parent, option, index);
 
-        QComboBox *box = new QComboBox(parent);
-        box->addItems(keys);
-        box->addItem("<custom>");
+		QComboBox *box = new QComboBox(parent);
+		box->addItems(keys);
+		box->addItem("<custom>");
 
-        return box;
-    }
-    virtual void setEditorData(QWidget *editor, const QModelIndex &index) const {
-        QComboBox *box = qobject_cast<QComboBox*>(editor);
-        if(box) {
-            QString currentText = index.data(Qt::EditRole).toString();
-            int boxIndex = box->findText(currentText);
-            if(boxIndex >= 0)
-                box->setCurrentIndex(boxIndex);
-        }
+		return box;
+	}
+	virtual void setEditorData(QWidget *editor, const QModelIndex &index) const {
+		QComboBox *box = qobject_cast<QComboBox*>(editor);
+		if(box) {
+			QString currentText = index.data(Qt::EditRole).toString();
+			int boxIndex = box->findText(currentText);
+			if(boxIndex >= 0)
+				box->setCurrentIndex(boxIndex);
+		}
 
-        else
-            QStyledItemDelegate::setEditorData(editor, index);
-    }
-    virtual void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const {
-        QComboBox *box = qobject_cast<QComboBox *>(editor);
-        if(box)
-            model->setData(index, box->currentText(), Qt::EditRole);
-        else
-            QStyledItemDelegate::setModelData(editor, model, index);
-    }
+		else
+			QStyledItemDelegate::setEditorData(editor, index);
+	}
+	virtual void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const {
+		QComboBox *box = qobject_cast<QComboBox *>(editor);
+		if(box)
+			model->setData(index, box->currentText(), Qt::EditRole);
+		else
+			QStyledItemDelegate::setModelData(editor, model, index);
+	}
 
 private:
-    QStringList keys;
+	QStringList keys;
 };
 
 ProjectSettingsTab::ProjectSettingsTab(Project* project, MainWindow* mainWindow)
-    : QWidget(mainWindow), TabbedWidget(this, mainWindow), m_block(false)
+	: QWidget(mainWindow), TabbedWidget(this, mainWindow), m_block(false)
 {
-    setupUi(this);
+	setupUi(this);
 
-    ui_settingsTable->setItemDelegate(new ComboDelegate(ui_settingsTable, CompilerManager::ref().keys()));
+	ui_settingsTable->setItemDelegate(new ComboDelegate(ui_settingsTable, CompilerManager::ref().keys()));
 
-    setAssociatedProject(project);
-    reload();
+	setAssociatedProject(project);
+	reload();
 }
 
 void ProjectSettingsTab::activate()
 {
-    QList<Menuable*> menus = mainWindow()->menuablesExcept(mainWindow()->standardMenus());
-    foreach(Menuable* menu, menus) {
-        ActivatableObject* activatable = dynamic_cast<ActivatableObject*>(menu);
-        if(activatable) activatable->setActive(0);
-    }
+	QList<Menuable*> menus = mainWindow()->menuablesExcept(mainWindow()->standardMenus());
+	foreach(Menuable* menu, menus) {
+		ActivatableObject* activatable = dynamic_cast<ActivatableObject*>(menu);
+		if(activatable) activatable->setActive(0);
+	}
 
-    mainWindow()->showProjectDock();
+	mainWindow()->showProjectDock();
 }
 
 bool ProjectSettingsTab::beginSetup()
 {
-    return true;
+	return true;
 }
 
 void ProjectSettingsTab::completeSetup()
 {
-    mainWindow()->setTabName(this, associatedProject()->name() + " Settings");
-    connect(associatedProject(), SIGNAL(settingsChanged()), SLOT(reload()));
+	mainWindow()->setTabName(this, associatedProject()->name() + " Settings");
+	connect(associatedProject(), SIGNAL(settingsChanged()), SLOT(reload()));
 }
 
 bool ProjectSettingsTab::close()
 {
-    associatedProject()->sync();
-    return true;
+	associatedProject()->sync();
+	return true;
 }
 
 void ProjectSettingsTab::refreshSettings()
@@ -104,55 +104,55 @@ void ProjectSettingsTab::refreshSettings()
 
 void ProjectSettingsTab::reload()
 {
-    const QStringMap& settings = associatedProject()->settings();
+	const QStringMap& settings = associatedProject()->settings();
 
-    Log::ref().debug("Reloaded settings tab");
+	Log::ref().debug("Reloaded settings tab");
 
-    m_block = true;
+	m_block = true;
 
-    ui_settingsTable->clearContents();
-    ui_settingsTable->setRowCount(settings.keys().size());
-    quint16 i = 0;
-    foreach(const QString& key, settings.keys()) {
-        ui_settingsTable->setItem(i, 0, new QTableWidgetItem(key));
-        ui_settingsTable->setItem(i, 1, new QTableWidgetItem(settings[key]));
+	ui_settingsTable->clearContents();
+	ui_settingsTable->setRowCount(settings.keys().size());
+	quint16 i = 0;
+	foreach(const QString& key, settings.keys()) {
+		ui_settingsTable->setItem(i, 0, new QTableWidgetItem(key));
+		ui_settingsTable->setItem(i, 1, new QTableWidgetItem(settings[key]));
 
-        ++i;
-    }
+		++i;
+	}
 
-    m_block = false;
+	m_block = false;
 }
 
 void ProjectSettingsTab::on_ui_settingsTable_cellChanged(int row, int column)
 {
-    if(m_block) return;
+	if(m_block) return;
 
-    if(column == 0 && ui_settingsTable->item(row, column)->text() == "<custom>") {
-        bool accepted;
-        QString text = QInputDialog::getText(this, tr("Custom Key"), tr("Enter key:"), QLineEdit::Normal, "", &accepted);
-        if(accepted)
-            ui_settingsTable->setItem(row, column, new QTableWidgetItem(text));
-        else
-            ui_settingsTable->setItem(row, column, new QTableWidgetItem(""));
-    }
+	if(column == 0 && ui_settingsTable->item(row, column)->text() == "<custom>") {
+		bool accepted;
+		QString text = QInputDialog::getText(this, tr("Custom Key"), tr("Enter key:"), QLineEdit::Normal, "", &accepted);
+		if(accepted)
+			ui_settingsTable->setItem(row, column, new QTableWidgetItem(text));
+		else
+			ui_settingsTable->setItem(row, column, new QTableWidgetItem(""));
+	}
 
-    QTableWidgetItem* key = ui_settingsTable->item(row, 0);
-    QTableWidgetItem* value = ui_settingsTable->item(row, 1);
+	QTableWidgetItem* key = ui_settingsTable->item(row, 0);
+	QTableWidgetItem* value = ui_settingsTable->item(row, 1);
 
-    if(key && value) associatedProject()->updateSetting(key->text(), value->text());
+	if(key && value) associatedProject()->updateSetting(key->text(), value->text());
 }
 
 void ProjectSettingsTab::on_ui_add_clicked()
 {
-    ui_settingsTable->insertRow(ui_settingsTable->rowCount());
+	ui_settingsTable->insertRow(ui_settingsTable->rowCount());
 }
 
 void ProjectSettingsTab::on_ui_remove_clicked()
 {
-    int row = ui_settingsTable->currentRow();
-    QTableWidgetItem* key = ui_settingsTable->item(row, 0);
-    QString keyString = key ? key->text() : "";
-    ui_settingsTable->removeRow(row);
+	int row = ui_settingsTable->currentRow();
+	QTableWidgetItem* key = ui_settingsTable->item(row, 0);
+	QString keyString = key ? key->text() : "";
+	ui_settingsTable->removeRow(row);
 
-    associatedProject()->removeSetting(keyString);
+	associatedProject()->removeSetting(keyString);
 }
