547fb3cd9aa297295134ed007261b06bb832115a
nzrock95 <nzrock95@gmail.com>
2013-09-23 05:00:02 -0500

    Move to trash for Linux NOT supported

---
+++ src/platform_nix/system_utils.cpp
@@ -0,0 +1,13 @@
+#include "system_utils.hpp"
+
+using namespace kiss;
+
+bool SystemUtils::supportsMoveToTrash()
+{
+  return false;
+}
+
+bool SystemUtils::moveToTrash(const QString &path)
+{
+  return false;
+}
