d07c3feca4e1ae1bd6e5d2a105723b7027285d20
Nafis Zaman <nafis.zaman-1@ou.edu>
2013-12-20 01:06:37 -0600

    Updated ApplicationFinder to find cs2 instead of Computer and Simulator

--- src/application_finder.cpp
+++ src/application_finder.cpp
@@ -25,33 +25,19 @@ ApplicationFinder ApplicationFinder::platform()
     
 #ifdef Q_OS_MAC
     {
-      finder.addApplicationPath("computer", "/Applications/computer.app/Contents/MacOS/computer");
+      finder.addApplicationPath("cs2", "/Applications/cs2.app/Contents/MacOS/cs2");
       QDir c(QDir::current());
       c.cdUp();
       c.cdUp();
-      finder.addApplicationPath("computer", c.absoluteFilePath("computer.app/Contents/MacOS/computer"));
+      finder.addApplicationPath("cs2", c.absoluteFilePath("cs2.app/Contents/MacOS/cs2"));
       c.cdUp();
-      finder.addApplicationPath("computer", c.absoluteFilePath("computer.app/Contents/MacOS/computer"));
-    }
-    {
-      finder.addApplicationPath("ks2", "/Applications/ks2.app/Contents/MacOS/ks2");
-      QDir c(QDir::current());
-      c.cdUp();
-      c.cdUp();
-      finder.addApplicationPath("ks2", c.absoluteFilePath("ks2.app/Contents/MacOS/ks2"));
-      c.cdUp();
-      finder.addApplicationPath("ks2", c.absoluteFilePath("ks2.app/Contents/MacOS/ks2"));
+      finder.addApplicationPath("cs2", c.absoluteFilePath("cs2.app/Contents/MacOS/cs2"));
     }
 #elif defined(Q_OS_WIN)
     {
       QDir c(QDir::current());
       c.cdUp();
-      finder.addApplicationPath("computer", c.absoluteFilePath("computer/computer.exe"));
-    }
-    {
-      QDir c(QDir::current());
-      c.cdUp();
-      finder.addApplicationPath("ks2", c.absoluteFilePath("ks2/ks2.exe"));
+      finder.addApplicationPath("cs2", c.absoluteFilePath("cs2/cs2.exe"));
     }
 #endif
     return finder;
